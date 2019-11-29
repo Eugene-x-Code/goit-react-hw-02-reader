c1203c315333a0e5a4129be72f35923d1b1c52a0
Eugene-x-Code <evgeni.iv.xy@gmail.com>
2019-11-30 01:12:00 +0200

    Updates

---
+++ precache-manifest.4872a2ecfe06443d95dbb65aaf20fb71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c469b461f0cc0c81912c9cef70f8cc0d",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "033f059a321387c8bd04",
+    "url": "/goit-react-hw-02-reader/static/css/main.375d6777.chunk.css"
+  },
+  {
+    "revision": "44581860dd8ef138b4e4",
+    "url": "/goit-react-hw-02-reader/static/js/2.2ff0548d.chunk.js"
+  },
+  {
+    "revision": "033f059a321387c8bd04",
+    "url": "/goit-react-hw-02-reader/static/js/main.44078031.chunk.js"
+  },
+  {
+    "revision": "0da2320c246f0dcbed6e",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.21ebc90b.js"
+  }
+]);
